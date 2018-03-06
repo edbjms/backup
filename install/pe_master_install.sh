c8e3adf36b811dd96a72b89b00722df156527da2
eboutili <eric.boutilier@puppet.com>
2018-03-05 22:09:33 -0600

    Configure autosign for both agent nodes

--- install/pe_master_install.sh
+++ install/pe_master_install.sh
@@ -5,4 +5,5 @@ cd /opt/puppetlabs/bin
 ./puppet agent -t;./puppet agent -t
 ./puppet apply -e 'include pe_repo::platform::ubuntu_1604_amd64'
 echo puppetlabs | ./puppet-access login --username admin --lifetime 0
+printf "nyc-agent\nlon-agent\n" >> /etc/puppetlabs/puppet/autosign.conf
 ./puppet-code deploy production --wait
