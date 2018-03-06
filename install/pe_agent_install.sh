2e5d56390e3372ab9373f8cb74f7398ddcf11189
eboutili <eric.boutilier@puppet.com>
2018-03-06 17:32:40 -0600

    -

--- install/pe_agent_install.sh
+++ install/pe_agent_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Installation script for the Puppet agent
-curl -k https://pdx-master:8140/packages/current/install.bash | /usr/bin/sudo bash
+curl -k https://pdx-master:8140/packages/current/install.bash | /bin/bash
