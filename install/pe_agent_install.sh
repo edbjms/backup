f25dabdb6a9a060f7991f2e7fbc26962374c2da3
eboutili <eric.boutilier@puppet.com>
2018-03-06 17:01:58 -0600

    Use full path for /usr/bin/sudo

--- install/pe_agent_install.sh
+++ install/pe_agent_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Installation script for the Puppet agent
-curl -k https://pdx-master:8140/packages/current/install.bash | sudo bash
+curl -k https://pdx-master:8140/packages/current/install.bash | /usr/bin/sudo bash
