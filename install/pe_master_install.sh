3d2486e2c4a25a904ee63a7f4462e73b089c3933
eboutili <eric.boutilier@puppet.com>
2018-03-06 21:05:11 -0600

    Skip enabling auto-sign to see what happens

--- install/pe_master_install.sh
+++ install/pe_master_install.sh
@@ -3,6 +3,6 @@
 /root/puppet-enterprise/puppet-enterprise-installer -c /tmp/pe.conf
 cd /opt/puppetlabs/bin
 ./puppet apply -e 'include pe_repo::platform::ubuntu_1604_amd64'
-#echo puppetlabs | ./puppet-access login --username admin --lifetime 0
-printf "nyc-agent\nlon-agent\n" >> /etc/puppetlabs/puppet/autosign.conf
+echo puppetlabs | ./puppet-access login --username admin --lifetime 0
+#printf "nyc-agent\nlon-agent\n" >> /etc/puppetlabs/puppet/autosign.conf
 ./puppet-code deploy production --wait
