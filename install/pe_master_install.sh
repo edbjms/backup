f05ec62053346f38155e229933058240b37e26de
eboutili <eric.boutilier@puppet.com>
2018-03-06 20:01:15 -0600

    Omit including ubuntu_1604_amd64 agent pkg to see what happens

--- install/pe_master_install.sh
+++ install/pe_master_install.sh
@@ -2,8 +2,7 @@
 # Installation script for the Puppet master
 /root/puppet-enterprise/puppet-enterprise-installer -c /tmp/pe.conf
 cd /opt/puppetlabs/bin
-#./puppet agent -t;./puppet agent -t
-./puppet apply -e 'include pe_repo::platform::ubuntu_1604_amd64'
+#./puppet apply -e 'include pe_repo::platform::ubuntu_1604_amd64'
 echo puppetlabs | ./puppet-access login --username admin --lifetime 0
 printf "nyc-agent\nlon-agent\n" >> /etc/puppetlabs/puppet/autosign.conf
 ./puppet-code deploy production --wait
