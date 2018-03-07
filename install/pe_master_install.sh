6e145df82dd5dcc03255e503d553b4bcd3f7df70
eboutili <eric.boutilier@puppet.com>
2018-03-06 22:15:49 -0600

    Test happens to when there's a critical typo in pe.conf...

--- install/pe_master_install.sh
+++ install/pe_master_install.sh
@@ -2,6 +2,6 @@
 # Installation script for the Puppet master
 /root/puppet-enterprise/puppet-enterprise-installer -c /tmp/pe.conf
 cd /opt/puppetlabs/bin
-./puppet apply -e 'include pe_repo::platform::ubuntu_1604_amd64'
+./puppet apply -e 'include pe_repo::platform::ubuntu_1604_amd64' #not actually needed
 echo puppetlabs | ./puppet-access login --username admin --lifetime 0
-#./puppet-code deploy production --wait
+./puppet-code deploy production --wait
