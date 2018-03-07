7ce72718e40b028896a22f65e9873e86eea75ab4
eboutili <eric.boutilier@puppet.com>
2018-03-06 19:28:13 -0600

    skip agent runs in master install to see what happens

--- install/pe_master_install.sh
+++ install/pe_master_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Installation script for the Puppet master
-/root/puppet-enterprise/puppet-enterprise-installer -p /tmp/pe.conf
+/root/puppet-enterprise/puppet-enterprise-installer -c /tmp/pe.conf
 cd /opt/puppetlabs/bin
-./puppet agent -t;./puppet agent -t
+#./puppet agent -t;./puppet agent -t
 ./puppet apply -e 'include pe_repo::platform::ubuntu_1604_amd64'
 echo puppetlabs | ./puppet-access login --username admin --lifetime 0
 printf "nyc-agent\nlon-agent\n" >> /etc/puppetlabs/puppet/autosign.conf
